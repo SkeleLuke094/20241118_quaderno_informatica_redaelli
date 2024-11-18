c390d20cd34c23844b885fa29ebac619def0b8be
SkeleLuke094 <skeleluke094@gmail.com>
2024-11-18 11:14:55 +0100

    Update es2_action_page.php
    Aggiunta pulsanti per tornare indietro

--- es2_action_page.php
+++ es2_action_page.php
@@ -9,5 +9,15 @@ $chars = array("$char1", "$char2", "$char3");
 foreach ($chars as $x) {
     echo "$x <br>";
 }
-
 ?>
+
+<!-- Button link to previous page -->
+<form action="es2_main.html">
+    <input type="submit" value="Ritorna Indietro" style="cursor: pointer">
+</form>
+
+<br>
+<!-- Button link to index -->
+<form action="index.html">
+    <input type="submit" value="Ritorna nell'indice" style="cursor: pointer">
+</form>
