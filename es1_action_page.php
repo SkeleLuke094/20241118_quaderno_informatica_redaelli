7a7b8e0e636176b8a438950c132b9ef751dd19da
SkeleLuke094 <skeleluke094@gmail.com>
2024-11-18 11:13:46 +0100

    Update es1_action_page.php
    Aggiunta pulsanti per tornare indietro

--- es1_action_page.php
+++ es1_action_page.php
@@ -11,5 +11,14 @@ if ($user != "sus" || $password != "no") {
 }else{
     echo "Sei stato riconosciuto come " . $user . ". Fai quello che vuoi ora.";
 }
-
 ?>
+<!-- Button link to previous page -->
+<form action="es1_main.html">
+    <input type="submit" value="Ritorna Indietro" style="cursor: pointer">
+</form>
+
+<br>
+<!-- Button link to index -->
+<form action="index.html">
+    <input type="submit" value="Ritorna nell'indice" style="cursor: pointer">
+</form>
